9dccc21fd1c21eacfc199183a6be026ea833c9a0
Jessica Yu <yuj@ufl.edu>
2019-11-19 15:34:56 -0500

    first

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -2,27 +2,6 @@ import React from 'react';
 import logo from '../../assets/logo.svg';
 import './Home.css';
 
-<<<<<<< Updated upstream
-function Home() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                </a>
-            </header>
-        </div>
-    );
-}
-=======
 export const Home = () => (
     <React.Fragment>
         <Navigation />
@@ -33,6 +12,5 @@ export const Home = () => (
         <Media />
     </React.Fragment>
 )
->>>>>>> Stashed changes
 
 export default Home;
