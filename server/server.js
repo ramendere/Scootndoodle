b8db88a247e81381a624a54581dedf297165ad57
Nicole <nciar1@ufl.edu>
2019-12-04 20:03:55 -0500

    fixed server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,55 @@
-const express = require('./config/express.js')
+/*const express = require('./config/express.js')
 
 // Use env port or default
 const port = process.env.PORT || 5000;
 
 const app = express.init()
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
+*/
+
+let express = require('express');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let bodyParser = require('body-parser');
+let dbConfig = require('./config/config');
+
+// Express Route
+const productRoute = require('../server/routes/product.route')
+
+// Connecting mongoDB Database
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log('Database sucessfully connected!')
+},
+  error => {
+    console.log('Could not connect to database : ' + error)
+  }
+)
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cors());
+app.use('/products', productRoute)
+
+
+// PORT
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log('Connected to port ' + port)
+})
+
+// 404 Error
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
