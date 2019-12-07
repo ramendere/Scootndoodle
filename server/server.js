57faab951c310689f7066eb627c400ffe7095464
Nicole Ciar <Nicole@Nicole-Ciar.local>
2019-12-07 11:37:09 -0500

    createError

--- server/server.js
+++ server/server.js
@@ -33,10 +33,6 @@ const server = app.listen(port, () => {
   console.log('Connected to port ' + port)
 })
 
-// 404 Error
-app.use((req, res, next) => {
-  next(createError(404));
-});
 
 app.use(function (err, req, res, next) {
   console.error(err.message);
