9ed073057e1d2c3d8a36e5d62f677145dd0b42e3
Nicole Ciar <Nicole@Nicole-Ciar.local>
2019-12-07 11:57:04 -0500

    updated

--- server/server.js
+++ server/server.js
@@ -39,3 +39,11 @@ app.use(function (err, req, res, next) {
   if (!err.statusCode) err.statusCode = 500;
   res.status(err.statusCode).send(err.message);
 });
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
